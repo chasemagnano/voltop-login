3c80aa552a365dfbfe45a640249534ff9710160b
Chase Magnano <chasemagnano@Chases-MacBook-Pro-2.local>
2023-07-26 10:56:25 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState } from 'react';
 
-function App() {
+function Login() {
+  const [username, setUsername] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = () => {
+    if(username==="chase" && password==="voltop") {
+      alert("Successful login")
+    } else {
+      alert("no no you fool")
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="text"
+        placeholder="Username"
+        value={username}
+        onChange={e => setUsername(e.target.value)}
+        />
+        
+        <input
+        type="password"
+        placeholder="Password"
+        value={password}
+        onChange={e => setPassword(e.target.value)}
+        />
+      <button onClick={handleLogin}>Login</button>
+      
     </div>
   );
 }
 
-export default App;
+
+
+export default Login;
